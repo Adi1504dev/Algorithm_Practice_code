797103521390fd57f128a65496e7510aaa55c69e
Aditya Singh <karateilove@gmail.com>
2021-01-23 19:01:09 +0530

    code add
    max weighted path code in maze

---
+++ src/DP/WeigthedPath_AllDirections.java
@@ -0,0 +1,35 @@
+package DP;
+import java.util.HashMap;
+public class WeigthedPath_AllDirections {
+	private static HashMap<String ,Integer> m=new HashMap<String,Integer>();
+	public static int max_weighted_Path(int a[][],int i,int j)
+	{
+		String key=i+"|"+j;
+		if(i==0 && j==0)
+		return a[0][0];
+		if(!m.containsKey(key))
+		{
+			if(i==0)
+				m.put(key,max_weighted_Path(a,i,j-1)+ a[i][j]);
+			else if(j==0)
+				m.put(key,max_weighted_Path(a,i-1,j)+ a[i][j]);
+			else
+			m.put(key, a[i][j]+Math.max(max_weighted_Path(a,i-1,j-1),Math.max(max_weighted_Path(a,i-1,j), max_weighted_Path(a,i,j-1))));
+		}
+		return m.get(key);
+	}
+public static void main(String []args)
+{
+	int a[][]=new int[2][3];
+	int n = 2;
+    int m = 3;
+    a[0][0] = 100;
+    a[0][1] = -350;
+    a[0][2] = -200;
+    a[1][0] = -100;
+    a[1][1] = -300;
+    a[1][2] = 700;
+    
+    System.out.println(max_weighted_Path(a,n-1,m-1));
+}
+}
--- src/DP/test.java
+++ src/DP/test.java
@@ -8,25 +8,7 @@ public class test{
 
 	public static void main(String[] args)
 	{
-		String input="aaaabbBcddee";
-		HashMap<String,Integer> m=new HashMap<String,Integer>();
-		for(int i=0;i<input.length();i++)
-		{
-			String key=input.charAt(i)+"";
-			if(m.containsKey(key))
-				m.put(key, m.get(key)+1);
-			else
-				m.put(key, 1);
-			
-				
-		}
-		Iterator hmIterator = m.entrySet().iterator(); 
-		while(hmIterator.hasNext())
-		{
-			Map.Entry data=(Map.Entry)hmIterator.next();
-			System.out.print(data.getKey()+""+data.getValue());
-			
-		}
+		char []a= {}
 	}
 }
 
